f45cca2a7370b8628eb1e9d5163ec15ae05f459f
patel <parthpatel167@gmail.com>
2021-01-22 20:23:18 -0600

    few changes

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', 
+  { useNewUrlParser: true, 
+    useUnifiedTopology: true , 
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
  .then(() => console.log("MongoDB successfully connected"))
  .catch(err => console.log(err)); 
 // routes
